42e871cf730166a4f103fa1cd81c615f3479b954
T145 <T145@protonmail.com>
2019-09-15 00:55:37 -0700

    Basic bot for getting started

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # smashbot
 Discord bot for managing SSB content
+
+Rename `.env.txt` to `.env` and provide your client key.
+Then run `run.js` and you're on your way!
---
+++ run.js
@@ -0,0 +1,28 @@
+
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(process.env.CLIENT_ID);
