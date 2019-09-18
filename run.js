bd011c87c544f3ed7e7cbde872fdd6736c55d5db
T145 <T145@protonmail.com>
2019-09-18 15:06:50 -0700

    ELO rankings are a go!

--- run.js
+++ run.js
@@ -4,79 +4,78 @@ const sql = require('sqlite3').verbose();
 
 dotenv.config();
 
+// In a production environment, the database is always live
+let db = new sql.Database('./db/competition.db', sql.OPEN_READWRITE | sql.OPEN_CREATE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log(' [*] Established database connection.');
+}).run('CREATE TABLE competitors(name TEXT NOT NULL UNIQUE, bio TEXT DEFAULT n00b, wins INTEGER DEFAULT 0, losses INTEGER DEFAULT 0, perf_rate REAL DEFAULT 10000, global_rank INTEGER DEFAULT NULL, prev_tourn_rank INTEGER DEFAULT NULL)', (err) => {
+  if (err) {
+    console.log(' [!] Primary table exists; skipping creation.');
+    return;
+  }
+  console.log(' [*] The database is live!');
+});
+
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-function save(query, params) {
-  console.log(' [ ] Beginning query.');
-
-  new sql.Database('./db/competition.db', sql.OPEN_READWRITE | sql.OPEN_CREATE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(' [*] Established database connection.');
-  }).run('CREATE TABLE competitors(name TEXT NOT NULL UNIQUE, bio TEXT DEFAULT n00b, wins INTEGER DEFAULT 0, losses INTEGER DEFAULT 0, perf_rate REAL DEFAULT 10000, global_rank INTEGER DEFAULT NULL, prev_tourn_rank INTEGER DEFAULT NULL)', (err) => {
-    if (err) {
-      console.log(' [-] Primary table exists; skipping creation.');
-      return;
-    }
-  }).run(query, params, (err) => {
+function updateStandings(name, opponent, won) {
+  db.run(`UPDATE competitors SET ${ won ? 'wins = wins + 1' : 'losses = losses + 1' } WHERE name = '${name}'`, (err) => {
     if (err) {
       console.error(err.message);
       return;
     }
-    console.log(` [*] Query successful!`);
-  }).close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(' [*] Closed database connection.');
+    console.log(` [*] ${name} ${ won ? 'defeated' : 'lost to'} ${opponent}!`);
   });
 }
 
+function probability(elo, opponent_elo) {
+  return 1.0 * 1.0 / (1 + 1.0 * Math.pow(10, 1.0 * (elo - opponent_elo) / 400));
+}
+
 function elo(name, opponent, won) {
-  // set the wins
-  if (won) {
-    save(`UPDATE competitors SET wins = wins + 1 WHERE name = ${name}`);
-  } else {
-    save(`UPDATE competitors SET losses = losses + 1 WHERE name = ${name}`);
-  }
+  // ensure both players have their results pre-calculated, so the elo calculation isn't off-balanced by one loss
+  updateStandings(name, opponent, won);
+  updateStandings(opponent, name, !won);
 
-  // fetch the current elo rank, opponent's elo rank, wins & losses
-  var old_elo, opponent_elo, wins, losses;
-
-  new sql.Database('./db/competition.db', sql.OPEN_READONLY, (err) => {
+  db.all(`SELECT wins, losses, perf_rate FROM competitors WHERE name = '${name}' OR name = '${opponent}'`, (err, res) => {
     if (err) {
-      console.error(err.message);
-    }
-    console.log(' [*] Established database connection.');
-  }).run(`SELECT name, perf_rate, wins, losses WHERE name = ${name}`, (err, res) => {
-    if (err) {
-      console.error(err.message);
+      console.log(err.message);
       return;
     }
-    old_elo = res[1];
-    wins = res[2];
-    losses = res[3];
-    console.log(' [*] Fetched necessary competitor information.');
-  }).run(`SELECT name, perf_rate WHERE name = ${opponent}`, (err, res) => {
-    if (err) {
-      console.error(err.message);
-      return;
+
+    var K = 30,
+      pelo = res[0].perf_rate,
+      oelo = res[1].perf_rate;
+    var pa = probability(pelo, oelo);
+    var pb = probability(oelo, pelo);
+
+    if (won) {
+      pelo = pelo + K * (1 - pa);
+      oelo = oelo + K * (0 - pb);
+    } else {
+      pelo = pelo + K * (0 - pa);
+      oelo = oelo + K * (1 - pb);
     }
-    opponent_elo = res[1];
-  }).close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(' [*] Fetched necessary opponent information.');
+
+    db.run(`UPDATE competitors SET perf_rate = ${ pelo } WHERE name = '${ name }'`, (err) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log(` [*] ${ name }: Elo applied successfully!`);
+    });
+
+    db.run(`UPDATE competitors SET perf_rate = ${ oelo } WHERE name = '${ opponent }'`, (err) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log(` [*] ${ opponent }: Elo applied successfully!`);
+    });
   });
-
-  // apply the new elo
-  // https://en.wikipedia.org/wiki/Chess_rating_system
-  var new_elo = old_elo + 16 * (wins - losses + (0.5 * ((opponent_elo - old_elo) / 200)));
-
-  save(`UPDATE competitors SET perf_rate = ${new_elo} WHERE name = ${name}`);
 }
 
 /**
@@ -88,8 +87,15 @@ client.on('ready', () => {
 });
 
 function registerCompetitor(name) {
-  console.log(`Registering competitor: ${name}`)
-  save(`INSERT INTO competitors(name) VALUES(?)`, [`${name}`]);
+  console.log(` [ ] Registering competitor: ${name}`);
+
+  db.run(`INSERT INTO competitors(name) VALUES(?)`, [name], (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log(` [*] Registration successful!`);
+  });
 }
 
 // Create an event listener for messages
@@ -111,14 +117,28 @@ client.on('message', message => {
     // splice returns the deleted elements, so it needs to not be in the initializer
     params.splice(0, 1);
 
-    if (message.member.roles.some(r => ["Owner"].includes(r.name)) && cmd === 'register') {
-      registerCompetitor(params.join(' '));
+    if (message.member.roles.some(r => ["Owner"].includes(r.name))) {
+      if (cmd === 'register') {
+        registerCompetitor(params.join(' '));
+      } else if (cmd === 'elo') {
+        // TODO: Validate these params
+        console.log(params);
+        elo(params[0], params[1], params[2]);
+      }
     }
 
     if (cmd === 'bio') {
       const bio = params.join(' ');
-      save(`UPDATE competitors SET bio = '${bio}' WHERE name = '${name}'`);
-      message.member.guild.channels.find(c => c.name === 'discussion').send(`${name} updated their bio! \n~\n${bio}`)
+
+      db.run(`UPDATE competitors SET bio = '${bio}' WHERE name = '${name}'`, (err) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        console.log(` [*] Bio update successful!`);
+      });
+
+      message.member.guild.channels.find(c => c.name === 'discussion').send(`${name} updated their bio!\n\n${bio}`)
     }
   }
 });
